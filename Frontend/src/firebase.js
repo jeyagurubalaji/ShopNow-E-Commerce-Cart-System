9a82e995128d59a2d0b671c959030cac2939f488
jeyagurubalaji <jeyagurubalaji@gmail.com>
2026-08-26 12:44:20 +0530

    Refactor Firebase config to use environment variables
    Updated Firebase configuration to use environment variables for sensitive data.

--- Frontend/src/firebase.js
+++ Frontend/src/firebase.js
@@ -1,20 +1,15 @@
-// Import the functions you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBSNgi9tlSKISbdB6AgSo2OCrdqVePaStA",
-  authDomain: "shopnow-9c651.firebaseapp.com",
-  projectId: "shopnow-9c651",
-  storageBucket: "shopnow-9c651.firebasestorage.app",
-  messagingSenderId: "788219758793",
-  appId: "1:788219758793:web:45b1967e87b286e8306518",
-  measurementId: "G-02W7VQ2GNH",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication
 export const auth = getAuth(app);
